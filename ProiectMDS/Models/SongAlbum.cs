ab8e7639793d0366737f797c1d75ef5b6186d969
Eusebiu Timofte <eusebiu.timofte2@outlook.com>
2020-02-29 12:58:26 -0800

    first commit

---
+++ ProiectMDS/Models/SongAlbum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectMDS.Models
+{
+    public class SongAlbum
+    {
+        public int Id { get; set; }
+        public int SongId { get; set; }
+        public int AlbumId { get; set; }
+        public virtual Album Album { get; set; }
+        public virtual Song Song { get; set; }
+
+
+
+    }
+}
